441dded192b937721a24ba4fcf0c0d5c6d205a82
FrontYu <1303052820@qq.com>
2024-06-06 20:53:28 +0800

    update contact controller

--- src/main/java/com/tencent/wxcloudrun/controller/ImageController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/ImageController.java
@@ -69,6 +69,48 @@ public class ImageController {
         }
     }
 
+    @GetMapping("/contact/{contactId}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String contactId,HttpServletRequest request) {
+        try {
+            // 获取图片的资源路径
+            Resource resource = resourceLoader.getResource("classpath:static/contact/" +contactId+ ".jpg" );
+
+            // 检查资源是否存在
+            if (!resource.exists()) {
+                return ResponseEntity.notFound().build();
+            }
+
+            // 检查客户端是否发送了 If-None-Match 头
+            String ifNoneMatch = request.getHeader("If-None-Match");
+            String currentEtag = calculateEtag(resource); // 计算当前资源的 ETag
+
+            if (ifNoneMatch != null && ifNoneMatch.equals(currentEtag)) {
+                return ResponseEntity.status(HttpStatus.NOT_MODIFIED).build();
+            }
+
+            // 设置 Last-Modified 头
+            long lastModified = resource.lastModified();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setLastModified(lastModified);
+
+            System.out.println(currentEtag);
+            // 设置 ETag 头
+            headers.setETag("\""+currentEtag+"\"");
+
+            // 设置响应头信息
+            InputStream inputStream = resource.getInputStream();
+            byte[] imageData = IOUtils.toByteArray(inputStream);
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(imageData);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
     private String calculateEtag(Resource resource) throws IOException {
         // 此处可以使用资源内容的哈希值或其他算法来生成 ETag
         // 这里简单地使用文件的最后修改时间作为 ETag
