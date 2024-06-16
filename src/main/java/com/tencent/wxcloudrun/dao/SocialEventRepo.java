a1ae53ed9c97ac11c2f12ef5d81819c7f1a0316a
FrontYu <1303052820@qq.com>
2024-06-16 22:01:39 +0800

    migrate db

--- src/main/java/com/tencent/wxcloudrun/dao/SocialEventRepo.java
+++ src/main/java/com/tencent/wxcloudrun/dao/SocialEventRepo.java
@@ -2,6 +2,11 @@ package com.tencent.wxcloudrun.dao;
 
 import com.tencent.wxcloudrun.dto.SocialEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SocialEventRepo extends JpaRepository<SocialEvent,Integer> {
+    @Query(value = "select * from social_event where end_time > CURRENT_TIMESTAMP", nativeQuery = true)
+    List<SocialEvent> findAll();
 }
